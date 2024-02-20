7305c1c24e5e61735139ae8bf46c18afec570cca
中島 拓哉 <nakajimatakuya@next-group.jp>
2024-02-20 20:34:22 +0900

    add command setgit

--- commands/index.js
+++ commands/index.js
@@ -4,6 +4,7 @@ const [operation] = process.argv.slice(2);
 
 let Credential = require('../lib/credential');
 let Git = require('../lib/git');
+const log =  require('../lib/log');
 
 switch(operation) {
   case 'setup': {
@@ -12,6 +13,24 @@ switch(operation) {
     credential.write(`hoge: 1234`);
 
     let git = new Git;
+    if (!git.isInstalled) {
+      log.pass(`git files`)
+      log.reason(`because .git directory is not found. If you start git management later, run the 'npx sesame gitsetup' command at that time.`)
+      break;
+    }
+
+    git.addAttribute('config/credentials.yml.enc', 'diff=sesame');
+    git.addFilter('sesame', 'npx sesame_diff');
+    git.addIgnore('config/master.key');
+    break;
+  }
+  case 'setgit': {
+    let git = new Git;
+    if (!git.isInstalled) {
+      log.error(`.git directory is not found.`)
+      break;
+    }
+
     git.addAttribute('config/credentials.yml.enc', 'diff=sesame');
     git.addFilter('sesame', 'npx sesame_diff');
     git.addIgnore('config/master.key');
@@ -44,6 +63,9 @@ The following is a list of commands:
           config/master.key: The common key used to decrypt encrypted configuration files.
           .gitignore: A configuration file that defines exclusion patterns for git management. If it already exists, only the 'master.key' will be appended.
           .gitattributes: The specification is made to obtain the differences in the configuration file in decrypted form. If the file already exists, only an append is made.
+
+  setgit  This command sets up git-filter and ignore file.
+          The main case to use this is if the system was not git managed during setup.
 
   cat     The contents of the decrypted configuration file are displayed when this command is executed.
 
